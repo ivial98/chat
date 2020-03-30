e51b57ba0e9ed13ac82f2c3c59e35d45800ab292
ilya <ilyavialkov@gmail.com>
2020-03-30 12:27:37 +0300

    Updates

---
+++ precache-manifest.c8b74592932e065b067824d23e98b401.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1428e8fc3eb45043fff32aadb731119",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "77bb41322b9a4922db5d",
+    "url": "/chat/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "6577ce3c11abbab05490",
+    "url": "/chat/static/css/main.fa201d99.chunk.css"
+  },
+  {
+    "revision": "77bb41322b9a4922db5d",
+    "url": "/chat/static/js/2.0e5f34a0.chunk.js"
+  },
+  {
+    "revision": "6577ce3c11abbab05490",
+    "url": "/chat/static/js/main.a6194bd1.chunk.js"
+  },
+  {
+    "revision": "0c8b820a509f15ac1d20",
+    "url": "/chat/static/js/runtime-main.cf1ae3de.js"
+  },
+  {
+    "revision": "ccc218653fdeb7007757b8b8260cb075",
+    "url": "/chat/static/media/audio-blue.ccc21865.svg"
+  },
+  {
+    "revision": "368958cc937139bd8e65643229d5ac83",
+    "url": "/chat/static/media/call-blue-icon.368958cc.svg"
+  },
+  {
+    "revision": "dde8dddb953fd967b5eea3e93a0eaa54",
+    "url": "/chat/static/media/call-grey-icon.dde8dddb.svg"
+  },
+  {
+    "revision": "12c1e338335b4404d38246e30382ce7b",
+    "url": "/chat/static/media/chat-black-icon.12c1e338.svg"
+  },
+  {
+    "revision": "f2b6b911da39c922f4480ded5a0e1eba",
+    "url": "/chat/static/media/chat-blue-icon.f2b6b911.svg"
+  },
+  {
+    "revision": "44781a7d0a5755af8c4e529ae332a1e3",
+    "url": "/chat/static/media/chat-grey-icon.44781a7d.svg"
+  },
+  {
+    "revision": "64e3a549e8a5fa43d80a7423c55e0784",
+    "url": "/chat/static/media/details-pane-blue-icon.64e3a549.svg"
+  },
+  {
+    "revision": "ddac87bcf632a855052eb551aa647796",
+    "url": "/chat/static/media/file-blue.ddac87bc.svg"
+  },
+  {
+    "revision": "41e7f07928b9c18080add336b04e836e",
+    "url": "/chat/static/media/group-chat-blue-icon.41e7f079.svg"
+  },
+  {
+    "revision": "7939953943e4d99e195477b4db35a662",
+    "url": "/chat/static/media/group-chat-grey-icon.79399539.svg"
+  },
+  {
+    "revision": "86ea7e6725d04fe79de95680881195b5",
+    "url": "/chat/static/media/help-black-icon.86ea7e67.svg"
+  },
+  {
+    "revision": "81b6ebdfd975555d7bebc0f2eafb5abb",
+    "url": "/chat/static/media/image-blue.81b6ebdf.svg"
+  },
+  {
+    "revision": "ac4e5e40a8d693ef2d4a1b8738a25f9a",
+    "url": "/chat/static/media/more-blue-icon.ac4e5e40.svg"
+  },
+  {
+    "revision": "a9bb9e7f9a847e9eed6079a5b53e16d7",
+    "url": "/chat/static/media/more-grey-icon.a9bb9e7f.svg"
+  },
+  {
+    "revision": "d1851434a7d18f1bddcba0e211340850",
+    "url": "/chat/static/media/notification-black-icon.d1851434.svg"
+  },
+  {
+    "revision": "92d29b436d1a3124a38f1498e220016c",
+    "url": "/chat/static/media/people-blue-icon.92d29b43.svg"
+  },
+  {
+    "revision": "5b3e3d144461a4a9fa3ae41dab405547",
+    "url": "/chat/static/media/people-grey-icon.5b3e3d14.svg"
+  },
+  {
+    "revision": "421ef8017febfd2d24185b69d3b69337",
+    "url": "/chat/static/media/privacy-black-icon.421ef801.svg"
+  },
+  {
+    "revision": "b00d3682106c61d8c29361cd150c91c1",
+    "url": "/chat/static/media/report-black-icon.b00d3682.svg"
+  },
+  {
+    "revision": "9e7719dfb7205cf611d803cf7aad4479",
+    "url": "/chat/static/media/rounded-plus-grey-icon.9e7719df.svg"
+  },
+  {
+    "revision": "ee1802c60eb6394041cef9f0b96d539c",
+    "url": "/chat/static/media/search-grey-icon.ee1802c6.svg"
+  },
+  {
+    "revision": "70c145e8dabb8ec42dc7c4a6bf3c6c85",
+    "url": "/chat/static/media/send-blue-icon.70c145e8.svg"
+  },
+  {
+    "revision": "96fee613c7fb38725305d47fa61a91fe",
+    "url": "/chat/static/media/video-blue.96fee613.svg"
+  }
+]);
